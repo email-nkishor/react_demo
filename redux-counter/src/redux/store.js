2522fdcfeef7f5ee4c442c93eeec65bdbf50dd5d
nkishor <nkishor@nsf.org>
2023-11-03 18:48:48 +0530

    testing branch

--- redux-counter/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-// import { createStore } from 'redux';
-import { legacy_createStore as createStore } from 'redux';
-
-import counterReducer from './reducers';
-
-const store = createStore(counterReducer);
-
-export default store;
---
+++ testing/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Nand Kishor!!!</h1>
+      <h2>Create new react project...</h2>
+    </div>
+  );
+}
+
+export default App;
