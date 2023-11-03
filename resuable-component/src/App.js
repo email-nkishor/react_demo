ee103394efdeb6ec0dac163a99831b52a5aec208
nkishor <nkishor@nsf.org>
2023-11-03 22:38:42 +0530

    Added resuable button component

--- resuable-component/src/App.js
+++ resuable-component/src/App.js
@@ -1,12 +1,56 @@
-import './App.css';
+import Button from './Button';
+import HomeIcon from './HomeIcon';
+import PlusIcon from './PlusIcon';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Nand Kishor!!!</h1>
-      <h2>Create new react project...</h2>
-    </div>
-  );
+      <div id="app">
+       <section>
+         <h2>Filled Button (Default)</h2>
+         <p>
+           <Button>Default</Button>
+         </p>
+         <p>
+           <Button mode="filled">Filled (Default)</Button>
+         </p>
+       </section>
+       <section>
+         <h2>Button with Outline</h2>
+         <p>
+           <Button mode="outline">Outline</Button>
+         </p>
+       </section>
+       <section>
+         <h2>Text-only Button</h2>
+         <p>
+           <Button mode="text">Text</Button>
+         </p>
+       </section>
+       <section>
+         <h2>Button with Icon</h2>
+         <p>
+           <Button Icon={HomeIcon}>Home</Button>
+         </p>
+         <p>
+           <Button Icon={PlusIcon} mode="text">
+             Add
+           </Button>
+         </p>
+       </section>
+       <section>
+         <h2>Buttons Should Support Any Props</h2>
+         <p>
+           <Button mode="filled" disabled>
+             Disabled
+           </Button>
+         </p>
+         <p>
+           <Button onClick={() => console.log('Clicked!')}>Click me</Button>
+         </p>
+       </section>
+     </div>
+   );
 }
 
 export default App;
